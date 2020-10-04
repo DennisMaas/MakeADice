d456846e43f880cc47c1f469a35f727c5caa5d50
DennisMaas <mail@dennismaas.de>
2020-10-04 14:13:02 +0200

    add while not 6, try again and count tries

--- src/MakeADice.java
+++ src/MakeADice.java
@@ -1,12 +1,7 @@
 /**
- * Schreibe ein Programm welches eine ganzzahlige
- * Zufallszahl zwischen 1 und 6 berechnet.
- * Wird eine 3 gewürfelt, so soll der Text
- * "Gewonnen, du hast eine 3 gewürfelt." ausgegeben werden.
- * Bei 4, 5 und 6 wird der Text
- * "Du hast eine [gewürfelte Augen] gewürfelt und darfst es noch einmal versuchen." ausgegeben.
- * Bei allen anderen Zahlen soll der Text
- * "Leider verloren, du hast eine [gewürfelte Augen] gewürfelt." ausgegeben werden.
+ * Berechne in einer while-Schleife solange eine ganzzahlige Zufallszahl
+ * zwischen 1 und 6, bis eine 6 gewürfelt wurde.
+ * Gebe auf der Konsole aus, wieviele Versuche hierzu benötigt wurden.
  */
 
 public class MakeADice {
@@ -14,16 +9,17 @@ public class MakeADice {
     public static void main(String[] args) {
         int max = 6;
         int min = 1;
-        int rollsExpected = 6000;
-        int countSixes = 0;
+        int countTries = 1;
         int numberToGet = 6;
         int range = max - min + 1;
+        int number;
         //Math.random returns a double between 0.0 and <1.0
-        for (int i = 0; i < rollsExpected ; i++) {
-            int number = (int) (Math.random() * range) + min;
-            if (number == 6) countSixes++;
+        number = (int) (Math.random() * range) + min;
+        while (number != numberToGet) {
+            countTries++;
+            number = (int) (Math.random() * range) + min;
         }
-        System.out.println("In " + rollsExpected + " Würfen wurde " + countSixes + " mal die " + numberToGet + " gewürfelt.");
 
+        System.out.println("Es wurden " + countTries + " Versuche benötigt, um eine " + numberToGet + " zu würfeln.");
     }
 }
